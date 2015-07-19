0a04538a4df47b7c9b6c7856803e57b7b5715f16
Mike Davis <mike@lightcycle.org>
2015-07-19 14:00:45 -0500

    Using Java 8

--- src/main/java/org/lightcycle/alife/geneticpollen/Settings.java
+++ src/main/java/org/lightcycle/alife/geneticpollen/Settings.java
@@ -17,8 +17,8 @@ public class Settings {
 	public static final int BIRTH_COST = 75;
 	public static final int AGE_ENERGY_COST = 1;
 	public static final int SUNLIT_ENERGY_GAIN = 6;
-	public static final double MUTATION_PROB = 0.01;
-	public static final double MUTATION_RATE = 0.2;
+	public static final double MUTATION_PROB = 0.05;
+	public static final double MUTATION_RATE = 0.5;
 	public static final EnumSet<Bound> BOUNDRIES = EnumSet.of(Bound.BOTTOM);
 	public static final boolean GRAVITY = true;
 	public static final WrapMode WRAPMODE = WrapMode.X;
--- src/main/java/org/lightcycle/alife/geneticpollen/genetics/PhenotypeProvider.java
+++ src/main/java/org/lightcycle/alife/geneticpollen/genetics/PhenotypeProvider.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -21,76 +20,11 @@ public class PhenotypeProvider {
 		
 	public PhenotypeProvider(String packagePrefix) {
 		reflections = new Reflections(packagePrefix);
-	}
-	
-	private Set<Class> getImplementations(Class type) {
-		if (cachedImplementations == null) {
-			cachedImplementations = new HashMap<Class,Set<Class>>();
-		}
-		
-		Set<Class> implementations = cachedImplementations.get(type);
-		if (implementations == null) {
-			implementations = (Set)reflections.getSubTypesOf(type);
-			cachedImplementations.put(type, implementations);
-		}
-		return implementations;
-	}
-	
-	private Class getRandomImplementation(Class type, Iterator<Integer> geneticInput) throws PhenotypeProviderException {
-		Set<Class> implementations = getImplementations(type);
-		if (implementations == null || implementations.size() == 0) {
-			return null;
-		} else if (implementations.size() == 1) {
-			return (Class)implementations.toArray()[0];
-		} else {
-			if (!geneticInput.hasNext()) {
-				throw new PhenotypeProviderException("Reached end of genetic data.");
-			}
-			return (Class)implementations.toArray()[geneticInput.next() % implementations.size()];
-		}
-	}
-	
-	private Constructor[] getConstructors(Class type) {
-		if (cachedConstructors == null) {
-			cachedConstructors = new HashMap<Class, Constructor[]>();
-		}
-		
-		Constructor[] constructors = cachedConstructors.get(type);
-		if (constructors == null) {
-			constructors = type.getConstructors();
-			cachedConstructors.put(type, constructors);
-		}
-		return constructors;
-	}
-	
-	private Constructor getRandomConstructor(Class type, Iterator<Integer> geneticInput) throws PhenotypeProviderException {
-		Constructor[] constructors = getConstructors(type);
-		if (constructors == null || constructors.length == 0) {
-			return null;
-		} else if (constructors.length == 1) {
-			return constructors[0];
-		} else {
-			if (!geneticInput.hasNext()) {
-				throw new PhenotypeProviderException("Reached end of genetic data.");
-			}
-			return constructors[geneticInput.next() % constructors.length];
-		}
-	}
-	
-	private Class[] getParameters(Constructor constructor) {
-		if (cachedParameterTypes == null) {
-			cachedParameterTypes = new HashMap<Constructor, Class[]>();
-		}
-		
-		Class[] classes = cachedParameterTypes.get(constructor);
-		if (classes == null) {
-			classes = constructor.getParameterTypes();
-			cachedParameterTypes.put(constructor, classes);
-		}
-		return classes;
+		cachedImplementations = new HashMap<>();
+		cachedConstructors = new HashMap<>();
+		cachedParameterTypes = new HashMap<>();
 	}
 
-	
 	public <T> T getInstance(Iterator<Integer> geneticInput, Class<T> type) throws PhenotypeProviderException  {
 		if (type.isEnum()) {
 			// Choose an enumeration value
@@ -104,17 +38,17 @@ public class PhenotypeProvider {
 			Class<? extends T> newtype = null;
 			if (type.isInterface()) {
 				// For interface, choose a class implementation of the interface
-				newtype = getRandomImplementation(type, geneticInput);
+				newtype = getImplementation(type, geneticInput);
 				if (newtype == null) {
-					throw new PhenotypeProviderException("Found no implementations of interface " + type.getName());						
+					throw new PhenotypeProviderException("Found no implementations of interface " + type.getName());
 				}
 			} else {
 				// For class, choose that class
 				newtype = type;
 			}
-			
+
 			// Choose constructor to use
-			Constructor constructor = getRandomConstructor(newtype, geneticInput);
+			Constructor constructor = getConstructor(newtype, geneticInput);
 
 			// Determine constructor parameter values
 			Class[] parametertypes = getParameters(constructor);
@@ -133,14 +67,14 @@ public class PhenotypeProvider {
 						}
 						parameters[i] = new Boolean(geneticInput.next() % 2 == 0);
 					} else {
-						throw new PhenotypeProviderException("Don't know how to construct primative parameter type " + parametertypes[i].getName());						
+						throw new PhenotypeProviderException("Don't know how to construct primative parameter type " + parametertypes[i].getName());
 					}
 				} else {
 					// Recursively get class instance for a parameter
-					parameters[i] = getInstance(geneticInput, parametertypes[i]);					
+					parameters[i] = getInstance(geneticInput, parametertypes[i]);
 				}
 			}
-			
+
 			// Instantiate class instance
 			try {
 				return (T) constructor.newInstance(parameters);
@@ -149,5 +83,51 @@ public class PhenotypeProvider {
 				throw new PhenotypeProviderException("Failed to construct instance of " + newtype.getName(), exception);
 			}
 		}
+	}
+
+	private Class getImplementation(Class type, Iterator<Integer> geneticInput) throws PhenotypeProviderException {
+		Set<Class> implementations = getImplementations(type);
+		if (implementations == null || implementations.size() == 0) {
+			return null;
+		} else if (implementations.size() == 1) {
+			return (Class)implementations.toArray()[0];
+		} else {
+			if (!geneticInput.hasNext()) {
+				throw new PhenotypeProviderException("Reached end of genetic data.");
+			}
+			return (Class)implementations.toArray()[geneticInput.next() % implementations.size()];
+		}
+	}
+
+	private Constructor getConstructor(Class type, Iterator<Integer> geneticInput) throws PhenotypeProviderException {
+		Constructor[] constructors = getConstructors(type);
+		if (constructors == null || constructors.length == 0) {
+			return null;
+		} else if (constructors.length == 1) {
+			return constructors[0];
+		} else {
+			if (!geneticInput.hasNext()) {
+				throw new PhenotypeProviderException("Reached end of genetic data.");
+			}
+			return constructors[geneticInput.next() % constructors.length];
+		}
+	}
+
+	private Constructor[] getConstructors(Class type) {
+		return cachedConstructors.computeIfAbsent(type, t -> {
+			return type.getConstructors();
+		});
+	}
+
+	private Class[] getParameters(Constructor constructor) {
+		return cachedParameterTypes.computeIfAbsent(constructor, c -> {
+			return c.getParameterTypes();
+		});
+	}
+
+	private Set<Class> getImplementations(Class type) {
+		return cachedImplementations.computeIfAbsent(type, t -> {
+			return reflections.getSubTypesOf(type);
+		});
 	}
 }
